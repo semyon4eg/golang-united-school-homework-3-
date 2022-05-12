63a9b5b24a67a04d9feb3ee73c96fed0d4aa3d0a
Andrey Semenov <semenowav@gmail.com>
2022-05-12 22:25:51 +0300

    Fix switched to For instead of Range

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,10 +1,12 @@
 package homework
 
 func average(input [15]float32) (result float32) {
+	
 	var sum float32 = 0
 	
 	for _, val := range input {
 		sum += val
 	}
+
 	return sum/float32(len(input))
 }
--- task03-map.go
+++ task03-map.go
@@ -4,13 +4,15 @@ func sortMapValues(input map[int]string) (result []string) {
 	
 	var min int
 
-	for k := range input {
-		min = k
+	for i := 0; i < len(input); i++ {
+
 		for key := range input {
+			min = key
 			if key < min {
 				min = key
 			}
 		}
+
 		result = append(result, input[min])
 		delete(input, min)
 	}
